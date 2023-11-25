01fd28babaf588b7a6466bdbd3278f0d39700e46
Varun <barunpandit143@gmail.com>
2023-11-25 16:02:26 +0530

    mail send update 25-11-2023

--- app/Http/Controllers/Admin/Order/AdminOrderController.php
+++ app/Http/Controllers/Admin/Order/AdminOrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin\Order;
 
 use App\Helpers\Classes\EasyData;
 use App\Http\Controllers\Controller;
-use App\Mail\SendInvoice;
+use App\Mail\SendInvoice2;
 use App\Models\Address;
 use App\Models\Morder;
 use App\Models\OrderedItem;
@@ -78,7 +78,7 @@ class AdminOrderController extends Controller
 
             $pdf_name = $datalist->first()->orderid . '.pdf';
             $pdf = PDF::loadView('admin.morder.invoice', compact('datalist'))->output();
-            // Mail::to('jisvishalkumar@gmail.com')->send(new SendInvoice2(compact('datalist', 'pdf', 'pdf_name')));
+            Mail::to('jisvishalkumar@gmail.com')->send(new SendInvoice2(compact('datalist', 'pdf', 'pdf_name')));
             $x->status(true);
             $x->message('Invoice has been sent.');
             return $x->json_output();
--- resources/views/admin/emails/send_invoice2.blade.php
+++ resources/views/admin/emails/send_invoice2.blade.php
@@ -4,12 +4,12 @@
 Your IOA invoice for your order is available. Please find the PDF document attached at the bottom of this email.
 <br>
 <br>
-Name : {{$data->username}}
+Name : {{$data->first()->address->name}}
 <br>
-Invoice No : {{$data->invoice_no}}
+Invoice No : {{$data->first()->orderid}}
 <br>
-Order Date : {{date('d/m/Y',strtotime($data->created_at))}}
+Order Date : {{date('d/m/Y',strtotime($data->first()->created_at))}} 
 <br>
-Payment ID : {{$data->payment_id}}
+Payment ID : {{$data->first()->payment_id}}
 
 @endcomponent
